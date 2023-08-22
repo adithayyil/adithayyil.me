dea57a4587f17ca9506607ef0c75a6e5944322a6
adi <thayyiladi@gmail.com>
2023-08-22 18:55:51 -0400

    responsive card text

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -16,11 +16,15 @@ function ProjectCard({
 }: ProjectCardProps) {
   return (
     <div className="w-full p-1">
-      <div className="bg-stone-950 border border-white rounded-lg shadow-md p-6">
-        <h3 className="text-white text-lg font-semibold mb-2">{title}</h3>
-        <p className="text-gray-300">{description}</p>
-        <TechTablets skills={skills} />
-        {githubLink && <ProjectLinkButton link={githubLink} />}
+      <div className="bg-stone-950 border border-white rounded-lg shadow-md p-6 ">
+        <h3 className="text-white text-lg font-semibold mb-2 sm:text-xl lg:text-2xl">
+          {title}
+        </h3>
+        <div className="text-sm sm:text-sm lg:text-base">
+          <p className="text-gray-300">{description}</p>
+          <TechTablets skills={skills} />
+          {githubLink && <ProjectLinkButton link={githubLink} />}
+        </div>
       </div>
     </div>
   );
